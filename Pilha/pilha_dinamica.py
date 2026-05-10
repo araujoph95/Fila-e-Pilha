6f2bc1a0a970d82d899eba18ba6ffaff1cb6e446
araujo.pehenrique <araujoferreirapedrohenrique@gmail.com>
2026-05-10 16:42:03 -0300

    Update pilha_dinamica.py

--- Pilha/pilha_dinamica.py
+++ Pilha/pilha_dinamica.py
@@ -8,4 +8,28 @@ class Pilha_dinamica:
         self.prim = None
         self.quant = 0
 
+    def show(self):
+        aux = self.prim
+        while aux != None:
+            print(aux.info, end=" ")
+            aux = aux.prox
+        print
+
+    def push(self, valor):
+        novo = No(valor, self.prim)
+        self.prim = novo 
+        self.quant += 1
+
+    def pop(self):
+        if self.quant == 0:
+            return
+        self.prim = self.prim.prox
+        self.quant -= 1
+
+    def get_topo(self):
+        if self.quant == 0: return
+        return self.prim.info
+    
+    def esta_vazia(self):
+        return self.quant == 0
     
